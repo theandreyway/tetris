dbbcaf2f5ba30c3bff50a687e44c935fb01c2492
Andrey Sklyar <theandreyway@gmail.com>
2018-06-02 23:42:06 -0700

    Display a grid of blocks that can be on or off.

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>
+          <div className="board">
+            <div className="row">
+              <div className="cell"/>
+              <div className="cell on"/>
+              <div className="cell"/>
+            </div>
+            <div className="row">
+              <div className="cell"/>
+              <div className="cell"/>
+              <div className="cell"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
